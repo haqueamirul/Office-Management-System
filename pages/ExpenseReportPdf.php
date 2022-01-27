59bf369ddd07348ee8152094083fe9e51e23b1bc
haqueamirul <haqueamirul76@gmail.com>
2022-01-27 17:43:33 +0600

    Office management by Amirul Haque

---
+++ pages/ExpenseReportPdf.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<H1>Buy Full Version</H1>
+	<p style="font-size:20px;">You can export after buying full version <br> Mail me at waqasoghi446@gmail.com</p>
+</body>
+</html>
